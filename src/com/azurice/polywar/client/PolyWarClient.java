c9feee2c0cff5c1537d978076304f1774bdff83d
Azur冰弦 <973562770@qq.com>
2022-07-08 17:30:58 +0800

    Basic packet & network

--- src/com/azurice/polywar/client/PolyWarClient.java
+++ src/com/azurice/polywar/client/PolyWarClient.java
@@ -1,29 +1,48 @@
 package com.azurice.polywar.client;
 
 import com.azurice.polywar.client.ui.MainWindow;
+import com.azurice.polywar.network.PingPacket;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
 
 public class PolyWarClient {
     private static final String SERVER_IP = "127.0.0.1";
     private static final int SERVER_PORT = 7777;
+    private static final InetSocketAddress SERVER_ADDRESS = new InetSocketAddress(SERVER_IP, SERVER_PORT);
     public static int TICK_RATE = 20;
     public static int FRAME_RATE = 144;
 
+    private static final Logger LOGGER = LogManager.getLogger("PolyWarClient");
 
     // Singleton
     private static final PolyWarClient instance = new PolyWarClient();
-    public double fps;
 
     public static PolyWarClient getInstance() {
         return instance;
     }
 
-    private MainWindow window;
-
     private PolyWarClient() {
     }
 
+
+    public double fps;
+    public double ms;
     private boolean running = true;
-//    private boolean stopped = true;
+    //    Socket socket;
+//    public boolean connected = false;
+    SocketChannel socketChannel;
+    ByteBuffer buf = ByteBuffer.allocate(1024);
+    private MainWindow window;
+    private boolean connected = false;
+
+    public boolean isConnected() {
+        return connected;
+    }
 
     // Runtime control
     public void run() {
@@ -31,18 +50,55 @@ public class PolyWarClient {
         window.display();
 
         while (running) {
-            // TODO: connect to server
+            if (!connected) {
+                try {
+                    LOGGER.info("Creating SocketChannel...");
+                    socketChannel = SocketChannel.open();
+                    LOGGER.info("Connecting to Server...");
+                    socketChannel.connect(SERVER_ADDRESS);
+                    connected = true;
+                    LOGGER.info("Connected");
+                    // TODO:
+                } catch (IOException e) {
+                    connected = false;
+                    LOGGER.error("Cannot connect to server: {}, retrying in 1 second...", e.toString());
+//                throw new RuntimeException(e);
+                }
+            } else {
+                try {
+                    ByteBuffer buf = new PingPacket().toByteBuffer();
+                    while (buf.hasRemaining()) {
+                        socketChannel.write(buf);
+                    }
+                } catch (IOException e) {
+                    LOGGER.error("Write error: ", e);
+                    connected = false;
+//                    throw new RuntimeException(e);
+                }
+            }
+
             try {
-                Thread.sleep(1000);
+                Thread.sleep(3000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
+            }
+        }
+
+        LOGGER.info("Closing...");
+        if (socketChannel != null) {
+            try {
+                LOGGER.info("Closing SocketChannel...");
+                socketChannel.close();
+            } catch (IOException e) {
+                LOGGER.error("Cannot close SocketChannel: {}", e.toString());
+//                            throw new RuntimeException(e);
             }
         }
         onStopped();
     }
 
 
-    private void stop() {
+    public void stop() {
         onStop();
     }
 
