3af341897ef785d5228f263933f2d7df1713a64c
Azur冰弦 <973562770@qq.com>
2022-07-08 18:50:28 +0800

    Cleanup

--- src/com/azurice/polywar/server/PolyWarServer.java
+++ src/com/azurice/polywar/server/PolyWarServer.java
@@ -2,7 +2,6 @@ package com.azurice.polywar.server;
 
 import com.azurice.polywar.network.Packet;
 import com.azurice.polywar.network.Util;
-import com.azurice.polywar.server.network.Handler;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -12,7 +11,10 @@ import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 
 public class PolyWarServer {
     ////// Constants //////
@@ -23,9 +25,7 @@ public class PolyWarServer {
     ///// Singleton //////
     private static PolyWarServer instance = new PolyWarServer();
     ////// Properties //////
-    List<Handler> handlers = Collections.synchronizedList(new ArrayList<>());
     ServerSocketChannel serverSocketChannel;
-    SocketChannel socketChannel;
     Selector selector;
     List<Room> rooms = Collections.synchronizedList(new ArrayList<>());
 
@@ -41,55 +41,49 @@ public class PolyWarServer {
     public void run() {
         this.running = true;
 
+        // Initialize Selector and ServerSocketChannel
         try {
-            LOGGER.info("Creating Selector");
+            LOGGER.info("Initializing network......");
+//            LOGGER.info("Creating Selector");
             selector = Selector.open();
 
-            LOGGER.info("Creating ServerSocketChannel...");
+//            LOGGER.info("Creating ServerSocketChannel...");
             serverSocketChannel = ServerSocketChannel.open();
             serverSocketChannel.bind(LISTEN_ADDRESS);
 
-            LOGGER.info("Registering ServerSocketChannel...");
+//            LOGGER.info("Registering ServerSocketChannel...");
             serverSocketChannel.configureBlocking(false);
             serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
-
         } catch (IOException e) {
-            LOGGER.error(e);
+            LOGGER.error("Network initialize failed", e);
             this.running = false;
         }
 
-        LOGGER.info("Handling for connection...");
+        LOGGER.info("Handling connections...");
         while (this.running) {
             try {
                 selector.select();
             } catch (IOException e) {
                 LOGGER.error("Select failed: ", e);
                 continue;
-//                throw new RuntimeException(e);
             }
 
-            Set<SelectionKey> selectionKeys = selector.selectedKeys();
-//            System.out.println(selectionKeys);
-            Iterator<SelectionKey> it = selectionKeys.iterator();
+            Iterator<SelectionKey> it = selector.selectedKeys().iterator();
             while (it.hasNext()) {
                 SelectionKey key = it.next();
                 it.remove();
 
                 if (key.isAcceptable()) { // OP_ACCEPT
-                    ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key.channel();
-//                    LOGGER.info("{} is Accept-ready", serverSocketChannel);
-                    handleAccept(serverSocketChannel);
-                } else if (key.isReadable()) { // OP_READ
-                    SocketChannel socketChannel = (SocketChannel) key.channel();
-//                    LOGGER.info("{} is Read-ready", socketChannel);
-                    handleRead(socketChannel);
+                    handleAccept((ServerSocketChannel) key.channel());
+                } else if (key.isReadable() & key.isWritable()) { // OP_READ
+                    handleRead((SocketChannel) key.channel());
                 }
             }
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
         }
 
         try {
@@ -97,7 +91,7 @@ public class PolyWarServer {
                 serverSocketChannel.close();
             }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            LOGGER.error("ServerSocketChannel close failed: ", e);
         }
     }
 
@@ -106,7 +100,7 @@ public class PolyWarServer {
             SocketChannel socketChannel = serverSocketChannel.accept();
             LOGGER.info("Accepted Client<{}>, registering...", socketChannel);
             socketChannel.configureBlocking(false);
-            socketChannel.register(selector, SelectionKey.OP_READ);
+            socketChannel.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
         } catch (IOException e) {
             LOGGER.error("Accept failed: ", e);
         }
@@ -115,7 +109,11 @@ public class PolyWarServer {
     public void handleRead(SocketChannel socketChannel) {
         try {
             Packet packet = Util.getPacket(socketChannel);
-            if (packet == null) return;
+            if (packet == null) {
+                LOGGER.info("[{}] Closed", socketChannel.getRemoteAddress());
+                socketChannel.close();
+                return;
+            }
 
             LOGGER.info("[{}] Received {}: {}",
                     socketChannel.getRemoteAddress(),
@@ -124,17 +122,5 @@ public class PolyWarServer {
         } catch (IOException e) {
             LOGGER.error("Read failed: ", e);
         }
-    }
-
-
-    public void broadcast(String msg) {
-        for (int i = 0; i < handlers.size(); i++) {
-            handlers.get(i).sendMessage(msg);
-        }
-    }
-
-    public void removeHandler(Handler handler) {
-        LOGGER.info("Removing handler: {} ...", handler);
-        this.handlers.remove(handler);
     }
 }
