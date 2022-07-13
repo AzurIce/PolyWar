7e56a559079ae08d1000eff03e96b7f7eaed7794
Azur冰弦 <973562770@qq.com>
2022-07-13 22:52:26 +0800

    Changed gitignore,removed TestGameButton

--- src/com/azurice/polywar/client/ui/page/MainPage.java
+++ src/com/azurice/polywar/client/ui/page/MainPage.java
@@ -19,7 +19,7 @@ public class MainPage extends PerformanceOverlayedPage {
         imageBg = new ImageIcon(imageURL).getImage();
     }
 
-    JButton btnStartGame;
+//    JButton btnStartGame;
     JButton btnCreateRoom;
     JButton btnJoinRoom;
     JLabel labelTitle;
@@ -85,6 +85,7 @@ public class MainPage extends PerformanceOverlayedPage {
 
         JPanel row1 = new Row();
         row1.setOpaque(false);
+        row1.setPreferredSize(new Dimension(295, 60));
         labelYourName = new JLabel("Your Name:");
         labelYourName.setOpaque(false);
         labelName = new JLabel("Player");
@@ -95,6 +96,7 @@ public class MainPage extends PerformanceOverlayedPage {
 
         JPanel row2 = new Row();
         row2.setOpaque(false);
+        row2.setPreferredSize(new Dimension(295, 60));
         textName = new JTextField("Player");
         textName.setOpaque(false);
         btnName = new JButton("Confirm");
@@ -115,15 +117,15 @@ public class MainPage extends PerformanceOverlayedPage {
 
         rightPanel.add(row3);
 
-        JPanel row4 = new Row();
-        row4.setOpaque(false);
-        row4.setPreferredSize(new Dimension(295, 60));
+//        JPanel row4 = new Row();
+//        row4.setOpaque(false);
+//        row4.setPreferredSize(new Dimension(295, 60));
 //        row4.setBorder(new LineBorder(new Color(0xff0000)));
-        btnStartGame = new JButton("TestStart");
-        btnStartGame.setPreferredSize(new Dimension(285, 50));
-        row4.add(btnStartGame);
+//        btnStartGame = new JButton("TestStart");
+//        btnStartGame.setPreferredSize(new Dimension(285, 50));
+//        row4.add(btnStartGame);
 
-        rightPanel.add(row4);
+//        rightPanel.add(row4);
 
         add(rightPanel, BorderLayout.EAST);
     }
@@ -131,7 +133,7 @@ public class MainPage extends PerformanceOverlayedPage {
     @Override
     public void initListeners() {
         super.initListeners();
-        btnStartGame.addActionListener(e -> parent.setPage(MainWindow.Page.GAME_PAGE));
+//        btnStartGame.addActionListener(e -> parent.setPage(MainWindow.Page.GAME_PAGE));
         btnCreateRoom.addActionListener(e -> parent.client.sendPacket(new CreateRoomPacket()));
         btnJoinRoom.addActionListener(e -> parent.setPage(MainWindow.Page.ROOM_LIST_PAGE));
         btnName.addActionListener(e -> {
